6af4ad13d101506181756423d9393333cb416eba
Pierluigi Secco <pierluigi.secco@we-plus.eu>
2023-04-17 11:20:59 +0200

    Add project files.

---
+++ BlazorServerMAui.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServerMAui.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BlazorServerMAui.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerMAui.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
